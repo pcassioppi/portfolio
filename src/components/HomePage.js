377e5e3b1029476f7f59a246f88083be51fdbf1c
pcassioppi <pjcassioppi@gmail.com>
2021-03-02 14:09:32 -0600

    added tabs

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -19,6 +19,7 @@ export default class HomePage extends Component{
                 <Tab.Container id="left-tabs-example" defaultActiveKey="first">
                     <Row>
                         <Col sm={3}>
+                        <Card>
                         <Nav variant="pills" className="flex-column">
                             <Nav.Item>
                             <Nav.Link eventKey="first">Resume</Nav.Link>
@@ -27,8 +28,10 @@ export default class HomePage extends Component{
                             <Nav.Link eventKey="second">Projects</Nav.Link>
                             </Nav.Item>
                         </Nav>
+                        </Card>
                         </Col>
                         <Col sm={9}>
+                        <Card>
                         <Tab.Content>
                             <Tab.Pane eventKey="first">
                             <Resume />
@@ -37,7 +40,9 @@ export default class HomePage extends Component{
                             <Projects/>
                             </Tab.Pane>
                         </Tab.Content>
+                        </Card>
                         </Col>
+                        
                     </Row>
                 </Tab.Container>
                 </Card>
