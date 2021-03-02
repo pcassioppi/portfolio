b15e541bcd991f0461ab1b70fdda73128218692b
pcassioppi <pjcassioppi@gmail.com>
2021-03-02 16:49:01 -0600

    changing formatting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,7 +25,7 @@ export default class Projects extends Component{
                                         <Card.Text>     - Utilized React.js and URQL to query and manipulate data from the GraphQL API for the frontend, allowing users to see and add 
                                             data using a map that consumes the Google Maps API and is displayed in a user friendly React-Bootstrap UI</Card.Text>
                                         <Card.Footer>
-                                            <Button href="https://github.com/pcassioppi/TopMusic" variant="light"><img src="https://p.kindpng.com/picc/s/128-1280187_github-logo-png-github-transparent-png.png" width="60" height="30" alt=''/></Button>
+                                            <Button href="https://github.com/pcassioppi/TopMusic" variant="light"><img src="https://p.kindpng.com/picc/s/128-1280187_github-logo-png-github-transparent-png.png" width="60" height="35" alt=''/></Button>
                                         </Card.Footer>
                                     </Card.Body>
                                 </Card>
@@ -40,7 +40,7 @@ export default class Projects extends Component{
                                         <Card.Text>     - Utilized BeautifulSoup python library and MongoDB change streams to dynamically crawl users' Last.FM data and retrieve track analysis data from Spotify’s API</Card.Text>
                                         <Card.Text>     - Implemented Bootstrap framework to display crawled data in an intuitive UI when user made request to the Django RESTFUL API from the React.js frontend</Card.Text>
                                         <Card.Footer> Github
-                                            <Button href="https://github.com/pcassioppi/TopMusic" variant="info"><img src="https://p.kindpng.com/picc/s/128-1280187_github-logo-png-github-transparent-png.png" alt=''/></Button>
+                                            <Button href="https://github.com/pcassioppi/TopMusic" variant="light"><img src="https://p.kindpng.com/picc/s/128-1280187_github-logo-png-github-transparent-png.png" width="60" height="35" alt=''/></Button>
                                         </Card.Footer>
                                     </Card.Body>
                                 </Card>
