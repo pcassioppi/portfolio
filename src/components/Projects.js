b63c687138c93a094fd34920650b7813cfcff7ef
pcassioppi <pjcassioppi@gmail.com>
2021-03-02 15:39:58 -0600

    changing formatting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,23 +9,26 @@ export default class Projects extends Component{
 
     render(){
         return (
-            <Container bg="secondary">
+            <Container>
                 <Jumbotron className="text-center">
                         <div>Here are some brief summaries of a couple of personal projects I've done, and some of the technologies I've used.</div>
                         <div>If you want to see more about a certain project, you can click their corresponding tabs below.</div>
                 </Jumbotron>
-                <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                    <Tab eventKey="home" title="Main">
-                        <div>Test1</div>
-                    </Tab>
-                    <Tab eventKey="SongSpots" title="SongSpots">
-                        <div>Test2</div>
-                    </Tab>
-                    <Tab eventKey="TopMusic" title="TopMusic">
-                        <div>Test3</div>
-                    </Tab>
-                </Tabs>
-            
+                <Card >
+                    <Card.Body>
+                        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                            <Tab eventKey="home" title="Main">
+                                <div>Test1</div>
+                            </Tab>
+                            <Tab eventKey="SongSpots" title="SongSpots">
+                                <div>Test2</div>
+                            </Tab>
+                            <Tab eventKey="TopMusic" title="TopMusic">
+                                <div>Test3</div>
+                            </Tab>
+                        </Tabs>
+                    </Card.Body>
+                </Card>
             </Container>
         );
     
