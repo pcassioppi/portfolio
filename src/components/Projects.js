79fbb193e4ab2ef2c3ba48ef01b6f5dc7c8ff70f
pcassioppi <pjcassioppi@gmail.com>
2021-03-02 17:03:53 -0600

    changing formatting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -23,9 +23,9 @@ export default class Projects extends Component{
                                         <Card.Text>     - Full stack web application that uses Django, GraphQL, AWS RDS Postgres, React.js, and Google Maps API to allow users to 
                                             log songs and the locations they associate them with</Card.Text>
                                         <Card.Text>     - Developed a GraphQL API using Django and JWT user authentication to manipulate and query users’ individual data from Postgres 
-                                            database hosted on AWS Cloud Relational Database Service</Card.Text>
-                                        <Card.Text>     - Utilized React.js and URQL to query and manipulate data from the GraphQL API for the frontend, allowing users to see and add 
-                                            data using a map that consumes the Google Maps API and is displayed in a user friendly React-Bootstrap UI</Card.Text>
+                                            database hosted on AWS RDS</Card.Text>
+                                        <Card.Text>     - Used React.js and URQL to query and manipulate data from the GraphQL API for the frontend, allowing users to see and add 
+                                            data using a map from the Google Maps API, which is displayed in a user friendly React-Bootstrap UI</Card.Text>
                                         <Card.Footer>
                                             <Button href="https://github.com/pcassioppi/SongSpots" variant="light"><img src="https://p.kindpng.com/picc/s/128-1280187_github-logo-png-github-transparent-png.png" width="60" height="35" alt=''/></Button>
                                         </Card.Footer>
@@ -36,7 +36,6 @@ export default class Projects extends Component{
                                     <Card.Body>
                                         <Card.Header>
                                             <Card.Title>Last.FM Spotify Data Scraper</Card.Title>
-                                        
                                         </Card.Header>
                                         <Card.Text>     - Full stack web application using Django REST framework, MongoDB, and React.js</Card.Text>
                                         <Card.Text>     - Utilized BeautifulSoup python library and MongoDB change streams to dynamically crawl users' Last.FM data and retrieve track analysis data from Spotify’s API</Card.Text>
@@ -49,7 +48,26 @@ export default class Projects extends Component{
                                 
                             </Tab>
                             <Tab eventKey="SongSpots" title="SongSpots">
-                                <div>Test2</div>
+                                
+                                <h1>SongSpots: Song Location Tracker</h1>
+                                <h2>Technologies used: Python, JavaScript, Django, React.js, GraphQL (Graphene & URQL), Postgres, JWT Authentication, AWS RDS, Google Maps API</h2>
+                                <br />
+                                <h3>Why?</h3>
+                                <div>
+                                    This Web app was something I built because I wished something like it existed. I love listening to music, and when I listen to some songs, they take me back to a certain memory from my life. These memories
+                                     are associated with the place I was in when they happened, so I wanted to be able to pinpoint the exact (or rough) location I was at when I heard a certain song, and I wanted to be able to describe the
+                                      memory and what was so memorable about that song at that place and time.
+                                </div>
+                                <div>
+                                This project also helped me bring together new technologies that I have been teaching myself, and gave me great hands on experience with them. I plan to make this my more "long-term" project, so I will be 
+                                updating it with different features over time.
+                                </div>
+                                <h3>Overview</h3>
+                                <div>
+                                The application allows a user to create a profile and login. After that, they can add songs to their list, by clicking the location on the Google Maps interface and entering the songs title, artist, the date 
+                                the memory is from, and a description of the memory. The information is then sent to the database, and they can see their information displayed either by refreshing the page, or by navigating to their page of
+                                 existing songs. The songs are displayed as points on the map, and the user can hover over them to read their details. The users songs are also displayed in a list (without coordinates) on the side of the page.
+                                </div>
                             </Tab>
                             <Tab eventKey="TopMusic" title="TopMusic">
                                 <div>Test3</div>
