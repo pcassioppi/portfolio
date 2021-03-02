0a154ef24f3a4a69b2f1374038ab52bd43b8f7d5
pcassioppi <pjcassioppi@gmail.com>
2021-03-02 16:14:15 -0600

    changing formatting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,11 +13,27 @@ export default class Projects extends Component{
                 <Card >
                     <Card.Body>
                         <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
-                            <Tab eventKey="home" title="Main"  className="p-4">
+                            <Tab eventKey="home" title="Main"  className="p-6">
                                 {/* <Container> */}
                                 <Card bg="secondary" text="white">
                                     <Card.Body>
                                         <Card.Header>SongSpots Song Location Tracker | Personal Project</Card.Header>
+                                        <Card.Text>     - Built full stack web application using Django, GraphQL, AWS RDS Postgres, React.js, and Google Maps API that allows users to 
+                                            log songs and the locations they associate them with</Card.Text>
+                                        <Card.Text>     - Developed a GraphQL API using Django and JWT user authentication to manipulate and query users’ individual data from Postgres 
+                                            database hosted on AWS Cloud Relational Database Service</Card.Text>
+                                        <Card.Text>     - Utilized React.js and URQL to query and manipulate data from the GraphQL API for the frontend, allowing users to see and add 
+                                            data using a map that consumes the Google Maps API and is displayed in a user friendly React-Bootstrap UI</Card.Text>
+                                        <Card.Footer>
+                                            <Card.Link href="https://github.com/pcassioppi/SongSpots">Github
+                                            </Card.Link>
+                                        </Card.Footer>
+                                    </Card.Body>
+                                </Card>
+                                <Card bg="secondary" text="white">
+                                    <Card.Body>
+                                        <Card.Title>SongSpots Song Location Tracker</Card.Title>
+                                        <Card.Subtitle>Personal Project</Card.Subtitle>
                                         <Card.Text>     - Built full stack web application using Django, GraphQL, AWS RDS Postgres, React.js, and Google Maps API that allows users to 
                                             log songs and the locations they associate them with</Card.Text>
                                         <Card.Text>     - Developed a GraphQL API using Django and JWT user authentication to manipulate and query users’ individual data from Postgres 
