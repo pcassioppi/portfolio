51fdbb01bb40976317ceb70f3bc05d4ed7aee738
pcassioppi <pjcassioppi@gmail.com>
2021-03-02 14:47:24 -0600

    chaning around formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header(){
     return (
         <Navbar bg="dark" variant="dark" sticky="top">
             
-            <Navbar.Brand className="justify-content-center">Peter Cassioppi</Navbar.Brand>
+            <Navbar.Text className="justify-content-center">Peter Cassioppi</Navbar.Text>
             {/* <Link to={"/portfolio"} className="navbar-brand">
                 
                     Home
