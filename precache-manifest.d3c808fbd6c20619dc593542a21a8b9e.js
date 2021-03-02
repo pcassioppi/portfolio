a74b31db243ee0f033aad66f1c17e87b786daa3f
pcassioppi <pjcassioppi@gmail.com>
2021-03-02 11:10:27 -0600

    Updates

--- precache-manifest.d3c808fbd6c20619dc593542a21a8b9e.js
+++ precache-manifest.d3c808fbd6c20619dc593542a21a8b9e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6015a0083d7c3e349faf60d3189ca1b0",
+    "revision": "d53b550252fa97ede3fbdaafb44c8ad9",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
+    "revision": "df01098f039362ea43a8",
     "url": "/portfolio/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
-    "url": "/portfolio/static/js/main.10a1b4a7.chunk.js"
+    "revision": "df01098f039362ea43a8",
+    "url": "/portfolio/static/js/main.d4680135.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
