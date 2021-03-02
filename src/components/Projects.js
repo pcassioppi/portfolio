e47edaa1b9dc644487953ef3d8bd9d955a0c1b7c
pcassioppi <pjcassioppi@gmail.com>
2021-03-02 16:11:06 -0600

    changing formatting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,19 +12,19 @@ export default class Projects extends Component{
                 </Jumbotron>
                 <Card >
                     <Card.Body>
-                        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                        <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
                             <Tab eventKey="home" title="Main"  className="p-4">
                                 {/* <Container> */}
                                 <Card bg="secondary" text="white">
                                     <Card.Body>
                                         <Card.Header>SongSpots Song Location Tracker | Personal Project</Card.Header>
-                                        <Card.Text>- Built full stack web application using Django, GraphQL, AWS RDS Postgres, React.js, and Google Maps API that allows users to 
+                                        <Card.Text>     - Built full stack web application using Django, GraphQL, AWS RDS Postgres, React.js, and Google Maps API that allows users to 
                                             log songs and the locations they associate them with</Card.Text>
-                                        <Card.Text>- Developed a GraphQL API using Django and JWT user authentication to manipulate and query users’ individual data from Postgres 
+                                        <Card.Text>     - Developed a GraphQL API using Django and JWT user authentication to manipulate and query users’ individual data from Postgres 
                                             database hosted on AWS Cloud Relational Database Service</Card.Text>
-                                        <Card.Text>- Utilized React.js and URQL to query and manipulate data from the GraphQL API for the frontend, allowing users to see and add 
+                                        <Card.Text>     - Utilized React.js and URQL to query and manipulate data from the GraphQL API for the frontend, allowing users to see and add 
                                             data using a map that consumes the Google Maps API and is displayed in a user friendly React-Bootstrap UI</Card.Text>
-                                        <Card.Footer className="text-muted">https://github.com/pcassioppi/SongSpots</Card.Footer>
+                                        <Card.Footer  href="https://github.com/pcassioppi/SongSpots">Github</Card.Footer>
                                     </Card.Body>
                                 </Card>
                                 {/* </Container> */}
