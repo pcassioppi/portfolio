5fba85bf3f754a754e588662089b462546155117
pcassioppi <pjcassioppi@gmail.com>
2021-03-02 15:34:10 -0600

    changing formatting

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -13,7 +13,7 @@ export default class Resume extends Component{
                 <Jumbotron className="text-center">
                     {/* will want to reduce the size of the resume image eventually */}
                         <Card bg="secondary">
-                        <Card.Header>Resume</Card.Header>
+                            <Card.Header>Resume</Card.Header>
                             <Card.Img variant="top" src="https://raw.githubusercontent.com/pcassioppi/pages_files/gh-pages/resume/ResumeJPEG.jpg" fluid/>
                         </Card>
                         
