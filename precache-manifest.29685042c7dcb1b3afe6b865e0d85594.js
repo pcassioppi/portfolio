7e81a8a59f449a6e0fae26a9b623da25d2f6c91c
pcassioppi <pjcassioppi@gmail.com>
2021-03-03 20:14:57 -0600

    Updates

--- precache-manifest.29685042c7dcb1b3afe6b865e0d85594.js
+++ precache-manifest.29685042c7dcb1b3afe6b865e0d85594.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ffd579c1548b9b37da1a4c7990229c9b",
+    "revision": "2e6926c4282dec8e785f1f04a4b9a4b6",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "7e216dc826cfbef62b27",
+    "revision": "8bf5f5aa7a8ddb35b900",
     "url": "/portfolio/static/css/2.b0704b7a.chunk.css"
   },
   {
-    "revision": "578233ba8b80f2da105b",
+    "revision": "7e26703bae0416b64535",
     "url": "/portfolio/static/css/main.1aafa5a4.chunk.css"
   },
   {
-    "revision": "7e216dc826cfbef62b27",
-    "url": "/portfolio/static/js/2.ce5b35ed.chunk.js"
+    "revision": "8bf5f5aa7a8ddb35b900",
+    "url": "/portfolio/static/js/2.370fd192.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/portfolio/static/js/2.ce5b35ed.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.370fd192.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "578233ba8b80f2da105b",
-    "url": "/portfolio/static/js/main.c86f83ef.chunk.js"
+    "revision": "7e26703bae0416b64535",
+    "url": "/portfolio/static/js/main.c359825e.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
